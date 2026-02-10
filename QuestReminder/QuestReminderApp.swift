feece730e0f3539f16bcd42a9736c4c6ad00913f
Miguel Carretas <miguelmelocarretas@gmail.com>
2026-02-10 17:37:30 +0000

    Initial Commit

---
+++ QuestReminder/QuestReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestReminderApp.swift
+//  QuestReminder
+//
+//  Created by Miguel Carretas on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct QuestReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
